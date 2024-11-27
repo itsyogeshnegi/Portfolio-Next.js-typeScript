c1d085b01d973b4707bfe47a037e906b76c4a29c
yogeshnegi2 <yogesh.negi@wemonde.com>
2024-11-27 17:52:16 +0530

    Done

--- src/app/AboutPage/page.tsx
+++ src/app/AboutPage/page.tsx
@@ -29,7 +29,7 @@ const AboutPage: React.FC = () => {
     const { years, months } = calculateExperience();
 
   return (
-    <>
+    <div className="min-h-screen">
       <div className="w-full flex justify-center items-center py-10">
         <h1 className="text-5xl font-bold text-[#4831d4] font-Teko tracking-wide">
           About Me
@@ -45,28 +45,28 @@ const AboutPage: React.FC = () => {
           <p className="text-[#4831d4] text-2xl md:text-3xl font-extrabold font-Teko leading-snug mb-4 text-center">
             {`Hi, I'm Yogesh Negi, Nice to meet you.`}
           </p>
-          <p className="text-purple-900 text-lg leading-relaxed font-Teko mb-6">
+          <p className=" text-lg leading-relaxed  mb-6">
             I am a proficient Software Developer specializing in JavaScript,
             React, and Next.js. With a keen eye for detail and a passion for
             crafting efficient solutions, I thrive in the dynamic world of web
             development, constantly pushing boundaries and delivering
             exceptional results.
           </p>
-          <hr className="w-full border-gray-300 my-4 text-purple-900" />
-          <p className="text-lg  font-Teko mb-2 text-purple-900">
+          <hr className="w-full border-gray-300 my-4" />
+          <p className="text-lg   mb-2 ">
             <span className="font-bold">Experience: </span> {years} year
             {years !== 1 ? "s" : ""} and {months} month
             {months !== 1 ? "s" : ""}
           </p>
-          <p className="text-lg  font-Teko mb-2 text-purple-900">
+          <p className="text-lg  mb-2">
             <span className="font-bold">Location: </span> New Delhi, India
           </p>
-          <p className="text-md text-purple-900 font-Teko">
+          <p className="text-md ">
             Curious to learn more about me? Download my CV using the link above!
           </p>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
--- src/app/HomePage/page.tsx
+++ src/app/HomePage/page.tsx
@@ -4,6 +4,11 @@ import NavBar from "@/components/NavBar/NavBar";
 import Footer from "@/components/Footer/Footer";
 import Skills from "@/components/Skills/Skills";
 import Image from "next/image";
+import { FaLinkedin } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa";
+import { FaSquareTwitter } from "react-icons/fa6";
+import { MdOutlineAttachEmail } from "react-icons/md";
+import { FaSquareInstagram } from "react-icons/fa6";
 
 const HomePage: React.FC = () => {
   return (
@@ -27,26 +32,51 @@ const HomePage: React.FC = () => {
               Crafting solid and scalable frontend products with great user
               experiences is my passion.
             </p>
+            <br />
+            <a
+              href="/YogeshNegi-CV.pdf"
+              download
+              className="mt-6 px-6 py-3 my-3 border-2 border-[#ccf381] rounded-lg text-xl text-white bg-[#4831d4] font-bold shadow-md hover:bg-[#ccf381] hover:text-[#4831d4] transition-all duration-300"
+            >
+              Download CV
+            </a>
           </div>
 
           {/* Right Section */}
           <div className="md:w-1/2 max-md:py-5 flex flex-col items-center mt-10 md:mt-0">
-            <div className=" h-60 w-64 flex items-center justify-center border-2 border-[#ccf381] p-2">
+            <div className=" h-60 w-64 flex items-center justify-center">
               <Image
                 width={500}
                 height={500}
                 src="/my.jpg"
                 alt="Yogesh"
+                className="rounded-md"
                 style={{ backgroundColor: "transparent" }}
               />
             </div>
-            <a
-              href="/YogeshNegi-CV.pdf"
-              download
-              className="mt-6 px-6 py-3 border-2 border-[#ccf381] rounded-lg text-xl text-white bg-[#4831d4] font-bold shadow-md hover:bg-[#ccf381] hover:text-[#4831d4] transition-all duration-300"
-            >
-              Download CV
-            </a>
+            <div className="flex gap-5 py-5">
+              <a
+                href="https://www.linkedin.com/in/yogesh-negi-7b8568302/"
+                target="_blank"
+              >
+                <FaLinkedin className="text-[#ccf381] text-2xl" />
+              </a>
+              <a href="https://github.com/itsyogeshnegi" target="_blank">
+                <FaGithub className="text-[#ccf381] text-2xl" />
+              </a>
+              <a href="https://x.com/itsyogeshnegi" target="_blank">
+                <FaSquareTwitter className="text-[#ccf381] text-2xl" />
+              </a>
+              <a href="mailto:yyogesh.singh.negi@gmail.com" target="_blank">
+                <MdOutlineAttachEmail className="text-[#ccf381] text-2xl" />
+              </a>
+              <a
+                href="https://www.instagram.com/itsyogeshnegi/"
+                target="_blank"
+              >
+                <FaSquareInstagram className="text-[#ccf381] text-2xl" />
+              </a>
+            </div>
           </div>
         </div>
       </div>
